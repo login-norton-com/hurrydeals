d807805f42e3510fd548c1f536c01b9d416f7125
login-norton-com <fordken98@gmail.com>
2026-02-24 23:18:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,14 @@
 <title>hurrydeals | Best Deals Online</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <!-- Privacy-friendly analytics by Plausible -->
+<script async src="https://plausible.io/js/pa-q3zF3Up6x7Kue5ViUHly_.js"></script>
+<script>
+  window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
+  plausible.init()
+</script>
+
+
 
 <style>
 body{
